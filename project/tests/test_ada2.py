dd60f8c0963ca507d25fee5f50ed3d7a5c168e10
loother-home <luther13williams@gmail.com>
2026-04-19 15:53:39 -0600

    directory cleaned

--- project/tests/test_ada2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from models.adaboost_model import AdaBoostModel
-X = np.random.rand(100, 5)
-y = np.random.randint(0, 2, 100)
-model = AdaBoostModel(n_estimators=5, max_depth=1)
-model.fit(X, y)
-print("weights:", model.weights_)
-preds = model.predict_proba(X)
-print("preds:", preds[:5])
